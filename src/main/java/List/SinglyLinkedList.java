0c42db674d8b966c2c497f0d218c079c84e6b5cf
laon <laon198@gmail.com>
2021-12-27 15:16:41 +0900

    SinglyLinkedList 완성

---
+++ src/main/java/List/SinglyLinkedList.java
@@ -0,0 +1,102 @@
+package List;
+
+public class SinglyLinkedList<E> {
+
+    private static class Node<E>{
+        private E data;
+        private Node<E> next;
+
+        private Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node<E> getNext(){return next; }
+        public void setNext(Node<E> node){ next = node; }
+        public E getData(){return data;}
+        public void setData(E data){ this.data = data; }
+    }
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public SinglyLinkedList(){
+        head = new Node<>(null, null);
+        tail = head;
+        size = 0;
+    }
+
+    public int size(){ return size;}
+    public boolean isEmpty(){ return size==0;}
+
+    public void add(E data){
+        Node<E> newNode = new Node<>(data, tail.getNext());
+        tail.setNext(newNode);
+        tail = newNode;
+        size++;
+    }
+
+    public void add(E data, int idx) {
+        validAddingIndex(idx);
+
+        if(idx==size){
+            add(data);
+        }
+
+        Node<E> prevNode = findNode(idx-1);
+        Node<E> newNode = new Node<>(data, prevNode.getNext());
+        prevNode.setNext(newNode);
+        size++;
+    }
+
+    public E get(int idx){
+        validExistingIndex(idx);
+
+        return findNode(idx).getData();
+    }
+
+    public void set(E data, int idx){
+        validExistingIndex(idx);
+
+        Node<E> targetNode;
+        if(idx==size){
+            targetNode = tail;
+        }else{
+            targetNode = findNode(idx);
+        }
+
+        targetNode.setData(data);
+    }
+
+    public void remove(int idx){
+        validExistingIndex(idx);
+
+        Node<E> prevNode = findNode(idx-1);
+        Node<E> targetNode = prevNode.getNext();
+        prevNode.setNext(targetNode.getNext());
+        size--;
+    }
+
+    private Node<E> findNode(int idx){
+        int curIdx = -1;
+        Node<E> curNode;
+        for(curNode=head;
+                curNode.getNext()!=null && curIdx<idx;
+                    curNode=curNode.getNext(), curIdx++){}
+
+        return curNode;
+    }
+
+    private void validExistingIndex(int idx){
+        if(idx<0 || idx>size-1){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void validAddingIndex(int idx){
+        if(idx<0 || idx>size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+}
---
+++ src/test/java/List/SinglyLinkedListTest.java
@@ -0,0 +1,138 @@
+package List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SinglyLinkedListTest {
+    private SinglyLinkedList<Integer> list;
+
+    @BeforeEach
+    void setUp() {
+        list = new SinglyLinkedList<Integer>();
+    }
+
+    @Test
+    @DisplayName("맨앞에 원소 삽입")
+    public void addFirstTest(){
+        fillList(6);
+        list.add(-1, 0);
+        assertEquals(7, list.size());
+        assertEquals(-1, list.get(0));
+    }
+
+    @Test
+    @DisplayName("중간에 원소 삽입")
+    public void addMiddleTest(){
+        fillList(6);
+        list.add(-1, 3);
+        list.add(-2, 3);
+        assertEquals(8, list.size());
+        assertEquals(-1, list.get(4));
+        assertEquals(-2, list.get(3));
+    }
+
+    @Test
+    @DisplayName("맨뒤에 원소 삽입")
+    public void addLastTest(){
+        fillList(6);
+        list.add(-1);
+        list.add(-2);
+        assertEquals(-1, list.get(6));
+        assertEquals(-2, list.get(7));
+        assertEquals(1, list.get(0));
+        assertEquals(2, list.get(1));
+    }
+
+    @Test
+    @DisplayName("원소 얻어오는 테스트")
+    public void getTest(){
+        fillList(6);
+
+        for(int i=0; i<6; i++){
+            assertEquals(i+1, list.get(i));
+        }
+    }
+
+    @Test
+    @DisplayName("원소 얻기 테스트 실패")
+    public void getFailTest(){
+        IndexOutOfBoundsException e = assertThrows(
+                IndexOutOfBoundsException.class, ()->{
+                    list.get(4);
+                }
+        );
+
+        fillList(3);
+
+        IndexOutOfBoundsException e2 = assertThrows(
+                IndexOutOfBoundsException.class, ()->{
+                    list.get(5);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("원소 수정 테스트")
+    public void setTest(){
+        fillList(8);
+        list.set(-1, 4);
+        assertEquals(-1, list.get(4));
+        list.set(-2, 4);
+        assertEquals(-2, list.get(4));
+        assertEquals(8, list.size());
+    }
+
+    @Test
+    @DisplayName("맨앞 원소 삭제 테스트")
+    public void removeFirstTest(){
+        fillList(8);
+        list.remove(0);
+        assertEquals(2, list.get(0));
+        list.remove(0);
+        assertEquals(3, list.get(0));
+        assertEquals(6, list.size());
+    }
+
+    @Test
+    @DisplayName("중간 원소 삭제 테스트")
+    public void removeMiddleTest(){
+        fillList(8);
+        list.remove(4);
+        assertEquals(6, list.get(4));
+        list.remove(4);
+        assertEquals(7, list.get(4));
+        assertEquals(6, list.size());
+    }
+
+    @Test
+    @DisplayName("맨 끝 원소 삭제 테스트")
+    public void removeLastTest(){
+        fillList(8);
+        list.remove(7);
+        IndexOutOfBoundsException e1 = assertThrows(
+                IndexOutOfBoundsException.class,
+                ()->{
+                    list.get(7);
+                }
+        );
+
+        list.remove(6);
+        IndexOutOfBoundsException e2 = assertThrows(
+                IndexOutOfBoundsException.class,
+                ()->{
+                    list.get(6);
+                }
+        );
+
+        assertEquals(6, list.size());
+    }
+
+    private void fillList(int addSize){
+        for(int i=1; i<=addSize; i++){
+            list.add(i);
+        }
+    }
+}
