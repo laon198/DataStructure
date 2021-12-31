4361e8fa7cee5d066e893bd76c8f02e2e4af228d
laon <laon198@gmail.com>
2021-12-31 14:16:28 +0900

    ArrayQueue/create

---
+++ src/main/java/Queue/ArrayQueue.java
@@ -0,0 +1,74 @@
+package Queue;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue<E> implements Queue<E> {
+    private E[] elements;
+    private int front;
+    private int rear;
+    private int size;
+    private static final int defaultCapacity = 4;
+
+    public ArrayQueue(){
+        elements = (E[])new Object[defaultCapacity];
+        front = 0;
+        rear = 0;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public void enqueue(E data) {
+        if(size==elements.length-1){
+            resize();
+        }
+
+        rear = (rear+1) % elements.length;
+        elements[rear] = data;
+        size++;
+    }
+
+    @Override
+    public E getFirst() throws NoSuchElementException {
+        checkEmpty();
+        return elements[(front+1)%elements.length];
+    }
+
+    @Override
+    public E dequeue() throws NoSuchElementException {
+        E data = getFirst();
+        elements[(front+1)% elements.length] = null;
+        front = (front+1)% elements.length;
+        size--;
+        return data;
+    }
+
+    private void checkEmpty() throws NoSuchElementException{
+        if(isEmpty()){
+            throw new NoSuchElementException("queue is empty");
+        }
+    }
+
+    private void resize() {
+        E[] tmp = (E[]) new Object[elements.length*2];
+
+        for(int srcCursor=front, targetCursor=0;
+                targetCursor< elements.length;
+                    srcCursor=(srcCursor+1)% elements.length, targetCursor++){
+            tmp[targetCursor] = elements[srcCursor];
+        }
+
+        front = 0;
+        rear = size;
+        elements = tmp;
+    }
+}
---
+++ src/test/java/Queue/ArrayQueueTest.java
@@ -0,0 +1,57 @@
+package Queue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayQueueTest {
+    private Queue<Integer> queue;
+
+    @BeforeEach
+    public void setUp(){
+        queue = new ArrayQueue<Integer>();
+    }
+
+    @Test
+    @DisplayName("enqueue 테스트")
+    public void enqueueTest(){
+        int addSize = 10;
+        fillQueue(addSize);
+
+        assertEquals(addSize, queue.size());
+    }
+
+    @Test
+    @DisplayName("dequeue 테스트")
+    public void dequeueTest(){
+        int addSize = 10;
+        fillQueue(addSize);
+
+        for(int i=0; i<addSize; i++){
+            assertEquals(i, queue.dequeue());
+        }
+
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    @DisplayName("dequeue 실패 테스트")
+    public void dequeueFailTest(){
+        NoSuchElementException e = assertThrows(
+                NoSuchElementException.class,
+                ()->{
+                    queue.dequeue();
+                }
+        );
+    }
+
+    private void fillQueue(int addSize){
+        for(int i=0; i<addSize; i++){
+            queue.enqueue(i);
+        }
+    }
+}
